095ab3dd44b1a12fab0f9234576ad8e173468c37
Auditive <info@auditive.tokyo>
2025-03-14 08:49:11 +0900

    headerとfooterのサイズ調整

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer className="header-footer-common">
-      <p>© 2025 AUDITIVE. All rights reserved.</p>
+      <p className="text-lg">© 2025 AUDITIVE. All rights reserved.</p>
     </footer>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Header: React.FC = () => {
   return (
     <header className="header-footer-common">
-      <h1>AUDITIVE</h1>
-      <p>Dark, Minimal, Experimental, Techy Drum&Bass for Bass Addicts</p>
+      <h1 className="text-2xl font-bold mb-2">AUDITIVE</h1>
+      <p className="text-lg">Dark, Minimal, Experimental, Techy Drum&Bass for Bass Addicts</p>
     </header>
   );
 };
